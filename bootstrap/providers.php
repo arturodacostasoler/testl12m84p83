46cadc38617fb0331452096a5822772682ede815
Arturo Antonio DaCosta Soler <arturodacostasoler@gmail.com>
2025-02-28 19:02:27 -0500

    stancl/tenancy
    stancl/tenancy

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,9 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+
+    App\Providers\TenancyServiceProvider::class,
+
     App\Providers\Filament\TestPanelProvider::class,
     App\Providers\FolioServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
