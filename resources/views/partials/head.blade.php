f2bf973401066d3d5c4df2aaa0bc41d16a99ff1c
Arturo Antonio DaCosta Soler <arturodacostasoler@gmail.com>
2025-03-21 21:23:37 -0500

    TEST_L12_M84_P83
    TEST_L12_M84_P83

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? 'PA_L12_M84_P83' }}</title>
+<title>{{ $title ?? 'TEST_L12_M84_P83' }}</title>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>PA_L12_M84_P83</title>
+        <title>TEST_L12_M84_P83</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
